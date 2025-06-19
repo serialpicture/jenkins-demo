d3b2cd091a5875c2761ecec4df0b0342a0527b8d
serialpicture <collart.r@gmail.com>
2025-06-19 15:53:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ app = FastAPI()
 def read_root():
     return "Hello, World!"
 
-@app.get("/tapetonnomapresleslash/{name}", response_class=PlainTextResponse)
+@app.get("/tapetonnomapresleslashstp/{name}", response_class=PlainTextResponse)
 def read_user(name: str):
     return f"Hello {name}" 
